124c6c1fa0995fae420f543c27c0f4ecd1e8b7b6
hwani165 <seungyung.kang@gmail.com>
2025-10-16 14:38:53 +0900

    fix : hide mapsize inspector

--- Assets/_Scripts/Ksy/SO/MapDataSO.cs
+++ Assets/_Scripts/Ksy/SO/MapDataSO.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "MapDataSO", menuName = "Scriptable Objects/MapDataSO")]
 public class MapDataSO : ScriptableObject
 {
+    [HideInInspector]
     public sbyte MapSizeX = 7;
+    [HideInInspector]
     public sbyte MapSizeY = 7;
+    [HideInInspector]
     public MapData Map = new MapData(7, 7);
+    [HideInInspector]
     public TargetsData Targets = new TargetsData(7,7);
 }
--- Assets/_Scripts/Ksy/Tiles/Player.cs
+++ Assets/_Scripts/Ksy/Tiles/Player.cs
@@ -1,4 +1,6 @@
 using KSY.Manager;
+using Unity.Cinemachine;
+using UnityEngine;
 
 namespace KSY.Tile
 {
@@ -9,6 +11,13 @@ namespace KSY.Tile
             throw new System.NotImplementedException();
         }
 
+        private void OnEnable()
+        {
+            CinemachineCamera cam = GameObject.Find("PlayerCam").GetComponent<CinemachineCamera>();
+            CameraTarget target = new CameraTarget();
+            target.TrackingTarget = transform;
+            cam.Target = target;
+        }
         private void Start()
         {
             GameManager.Instance.InputManager.MoveKeyPressed += (x,y)=> Move(x,y);
