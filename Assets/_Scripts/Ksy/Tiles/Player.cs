6fd754b68b71a814a75f92257fca84133efaad7d
hwani165 <seungyung.kang@gmail.com>
2025-10-21 11:03:19 +0900

    save

--- Assets/_Scripts/Ksy/Tiles/Player.cs
+++ Assets/_Scripts/Ksy/Tiles/Player.cs
@@ -8,6 +8,7 @@ namespace KSY.Tile
     public class Player : TileObject
     {
         public Action<sbyte, sbyte> HandleMoveKeyPressed;
+        public CinemachineCamera cam;
         public override void Magnetization(int xDir, int yDir, TileObject presser)
         {
             //Debug.LogError($"{gameObject.name}의 Magnetization는 오버라이드되지 않았습니다.");
@@ -15,7 +16,6 @@ namespace KSY.Tile
 
         private void OnEnable()
         {
-            CinemachineCamera cam = GameObject.Find("PlayerCam").GetComponent<CinemachineCamera>();
             CameraTarget target = new CameraTarget();
             target.TrackingTarget = transform;
             cam.Target = target;
